737dfe3e96672360f50dfd0aff91a00ddc6d468f
Dan Matthews <dan@hydrant.co.uk>
2013-09-27 15:39:22 +0100

    First commit

---
+++ app/routes.php
@@ -0,0 +1,88 @@
+<?php
+
+use \Chumper\Zipper\Zipper;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function()
+{
+	return View::make('hello', [
+		'input' => Input::old()
+	]);
+});
+
+Route::post('process', function()
+{
+	$input = Input::all();
+
+	$rules = [
+		'module_name' => 'required|min:3',
+		'module_description' => 'required|min:3',
+	];
+	$validator = Validator::make($input, $rules);
+
+	if ($validator->fails()) {
+		return Redirect::to('/')->withErrors($validator)->withInput($input);
+	} else {
+
+		$name = strtolower(  str_replace([' '], ['_'], $input['module_name']) );
+		$description = $input['module_description'];
+		$version = (int)$input['drupal_version'];
+		$dependencies = explode(',', $input['dependencies']);
+
+		$info_file = View::make("d{$version}_info", [
+			'module_name' => $name,
+			'dependencies' => $dependencies,
+			'description' => $description,
+		])->render();
+
+		$tempfoldername = storage_path().'/modulename_'.$name;
+
+		if (!File::isDirectory($tempfoldername)) {
+			File::makeDirectory($tempfoldername);
+		} else {
+			File::cleanDirectory($tempfoldername);
+		}
+
+		File::put($tempfoldername."/{$name}.info", $info_file);
+
+		/** Module File **/
+		$module_file = View::make("module", [
+			'module_name' => $name,
+			'dependencies' => $dependencies,
+			'description' => $description,
+		])->render();
+
+		if (File::isDirectory($tempfoldername)) {
+			File::put($tempfoldername."/{$name}.module", $module_file);
+		}
+
+		/** Zip the thing up **/
+
+		$zipfilename = $tempfoldername."/zip_$name.zip";
+
+		if (File::exists($zipfilename)) {
+			File::delete($zipfilename);
+		}
+
+		$zipper = new Zipper();
+
+		$zipper->make($zipfilename)->add($tempfoldername);
+		if ($zipper->getStatus() == 'No error') {
+			$zipper->close();
+			return Response::download($zipfilename, "$name");
+		}
+
+		dd(Input::all());
+	}
+
+});
